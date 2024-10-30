1eafa283c813937bd6762609a2b940023db3dee8
Maria Fernanda <mfnanda.ribeiro@gmail.com>
2024-10-30 16:28:34 -0300

    inicio da nav bar de usuário

--- docker-laravel-vuejs-master/frontend/src/router/index.js
+++ docker-laravel-vuejs-master/frontend/src/router/index.js
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import App from '@/App.vue';
+import VagasView from '@/views/VagasView.vue';
+import PerfilView from '@/views/PerfilView.vue';
+import ApplicationView from '@/views/ApplicationView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,6 +16,30 @@ const router = createRouter({
         title: 'Home'
       }
     },
+    {
+      path: '/vagas',
+      name: 'vagas',
+      component: VagasView,
+      meta: {
+        title: 'Vagas'
+      }
+    },
+    {
+      path: '/perfil',
+      name: 'perfil',
+      component: PerfilView,
+      meta: {
+        title: 'Perfil'
+      }
+    },
+    {
+      path: '/application',
+      name: 'application',
+      component: ApplicationView,
+      meta: {
+        title: 'Application'
+      }
+    }
   ]
 })
 
