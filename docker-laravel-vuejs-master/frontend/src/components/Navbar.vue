5a6c63de26d7a96cb60c7fe87c863649bfda9fdd
Maria Fernanda <mfnanda.ribeiro@gmail.com>
2024-11-01 16:51:24 -0300

    tela de registro geral quase ok

--- docker-laravel-vuejs-master/frontend/src/components/Navbar.vue
+++ docker-laravel-vuejs-master/frontend/src/components/Navbar.vue
@@ -8,6 +8,7 @@
                 <router-link to="/">Home</router-link>
                 <router-link to="/login">Login</router-link>
                 <router-link to="/register">Registro</router-link>
+                <router-link to="/contato">Contato</router-link>
             </div>
         </nav>
     </div>
